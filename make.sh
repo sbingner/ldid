b135548dfe7993e0b26e72e025f9bed5533817f6
Sam Bingner <sam@bingner.com>
2018-09-17 08:46:39 -1000

    Make make.sh work on non-osx

--- make.sh
+++ make.sh
@@ -2,4 +2,14 @@
 
 set -e -x
 
-make clean all CC="xcrun gcc" CXX="xcrun g++" CFLAGS="-arch i386 -arch x86_64 -mmacosx-version-min=10.4"
+if which xcrun &>/dev/null; then
+	CC="xcrun gcc"
+	CXX="xcrun g++"
+	CFLAGS="-arch i386 -arch x86_64 -mmacosx-version-min=10.4"
+else
+	CC="gcc"
+	CXX="g++"
+	CFLAGS=""
+fi
+
+make clean all CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}"
